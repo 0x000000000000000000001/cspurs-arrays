2cf850893da9c7782f9558ead22ca1be816c718d
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:29:04 +0200

    feat: add C# FFI implementations

---
+++ src/Data/Array/ST.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Data.Array.ST;
+
+public static class FFI {
+    public static object New_(object arg1) => throw new NotImplementedException("Not implemented: new_");
+    public static object PeekImpl(object arg1, object arg2, object arg3, object arg4) => throw new NotImplementedException("Not implemented: peekImpl");
+    public static object PokeImpl(object arg1, object arg2, object arg3) => throw new NotImplementedException("Not implemented: pokeImpl");
+    public static object LengthImpl(object arg1) => throw new NotImplementedException("Not implemented: lengthImpl");
+    public static object PopImpl(object arg1, object arg2, object arg3) => throw new NotImplementedException("Not implemented: popImpl");
+    public static object PushAllImpl(object arg1, object arg2) => throw new NotImplementedException("Not implemented: pushAllImpl");
+    public static object ShiftImpl(object arg1, object arg2, object arg3) => throw new NotImplementedException("Not implemented: shiftImpl");
+    public static object UnshiftAllImpl(object arg1, object arg2) => throw new NotImplementedException("Not implemented: unshiftAllImpl");
+    public static object SpliceImpl(object arg1, object arg2, object arg3, object arg4) => throw new NotImplementedException("Not implemented: spliceImpl");
+    public static object UnsafeFreezeImpl(object arg1) => throw new NotImplementedException("Not implemented: unsafeFreezeImpl");
+    public static object UnsafeThawImpl(object arg1) => throw new NotImplementedException("Not implemented: unsafeThawImpl");
+    public static object FreezeImpl(object arg1) => throw new NotImplementedException("Not implemented: freezeImpl");
+    public static object ThawImpl(object arg1) => throw new NotImplementedException("Not implemented: thawImpl");
+    public static object CloneImpl(object arg1) => throw new NotImplementedException("Not implemented: cloneImpl");
+    public static object SortByImpl(object arg1, object arg2, object arg3) => throw new NotImplementedException("Not implemented: sortByImpl");
+    public static object ToAssocArrayImpl(object arg1) => throw new NotImplementedException("Not implemented: toAssocArrayImpl");
+    public static object PushImpl(object arg1, object arg2) => throw new NotImplementedException("Not implemented: pushImpl");
+}
---
+++ src/Data/Array/ST/Partial.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Data.Array.ST.Partial;
+
+public static class FFI {
+    public static object PeekImpl(object arg1, object arg2) => throw new NotImplementedException("Not implemented: peekImpl");
+    public static object PokeImpl(object arg1, object arg2, object arg3) => throw new NotImplementedException("Not implemented: pokeImpl");
+}
